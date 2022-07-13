1f79b990b056495490372576d609b9c03a5226e7
Dimostenis Nikolakopoulos <dn242@njit.edu>
2022-07-13 17:54:22 -0400

    Logo Bar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'DNDS';
+  darkMode={
+  	background : "black",
+  	text : "#fff",
+  	gradBckgrnd1 : "rgb(34,1,51)",
+  	gradBckgrnd2 :"linear-gradient(150deg, rgba(34,1,51,1) 0%, rgba(38,0,55,1) 55%, rgba(19,0,65,1) 82%, rgba(0,57,34,1) 100%)"
+  }
 }
