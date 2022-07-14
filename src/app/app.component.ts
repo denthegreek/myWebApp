f0db5db818e7bba76670ff4fde5a3459970aafee
Dimostenis Nikolakopoulos <dn242@njit.edu>
2022-07-13 22:23:16 -0400

    Nav Bar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,41 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'DNDS';
-  darkMode={
-  	background : "black",
-  	text : "#fff",
-  	gradBckgrnd1 : "rgb(34,1,51)",
-  	gradBckgrnd2 :"linear-gradient(150deg, rgba(34,1,51,1) 0%, rgba(38,0,55,1) 55%, rgba(19,0,65,1) 82%, rgba(0,57,34,1) 100%)"
-  }
+	title = 'DNDS';
+	navInfo:string = "";
+	activeInfo:string="";
+	whiteMode={
+		background : "#0000",
+		text : "purple",
+		gradBckgrnd1 : "rgb(34,1,51)",
+		gradBckgrnd2 :"linear-gradient(150deg, rgba(34,1,51,1) 0%, rgba(38,0,55,1) 55%, rgba(19,0,65,1) 82%, rgba(0,57,34,1) 100%)"
+	}
+	navClick(elementId:string){
+		let collection= Array.from(document.getElementsByClassName("activeCircle"));
+		console.log(collection);
+		collection.forEach(element => {
+			element.classList.remove("activeCircle");
+			element.classList.add("circle");
+		});
+
+		document.getElementById(elementId)!.classList.remove("circle");
+		document.getElementById(elementId)!.classList.add("activeCircle");
+		this.activeInfo="// "+elementId;
+		document.getElementById("position")!.style.color="#8C62F1";
+	}
+	navigateInfoChanger(info:string){
+		if(info!="" && info!=this.activeInfo){
+			this.navInfo=info;
+			document.getElementById("position")!.style.color="#1CC3E2";
+		}
+		else{
+			this.navInfo=this.activeInfo;
+			document.getElementById("position")!.style.color="#8C62F1";
+		}
+	}
 }
