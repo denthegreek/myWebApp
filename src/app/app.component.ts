cb0c37eef83fecb37f28cf362e29a7d1de372fbe
dnikolas1993 <dnikolas1993@gmail.com>
2022-07-16 11:28:08 -0400

    About Me

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,7 +11,7 @@ export class AppComponent {
 	activeInfo:string="";
 	whiteMode={
 		background : "#0000",
-		text : "purple",
+		text : "#8C62F1",
 		gradBckgrnd1 : "rgb(34,1,51)",
 		gradBckgrnd2 :"linear-gradient(150deg, rgba(34,1,51,1) 0%, rgba(38,0,55,1) 55%, rgba(19,0,65,1) 82%, rgba(0,57,34,1) 100%)"
 	}
