3e8bef974cc601f7193742b6336ffcc9c44fd2e3
dnikolas1993 <dnikolas1993@gmail.com>
2022-07-21 00:30:54 -0400

    About Me

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 	title = 'DNDS';
-	navInfo:string = "";
+	navInfo:string = "// about me";
 	activeInfo:string="// about me";
 
 	assetsPath="/myWebApp/";
