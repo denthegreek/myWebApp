93160db420aab32f0f9fe9faf2832c54f2c61548
dnikolas1993 <dnikolas1993@gmail.com>
2022-07-21 23:32:38 -0400

    Finalizing About Me

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -43,17 +43,20 @@ export class AppComponent {
 	}
 	scroller:number=0;
 	scrollCount:number=0;
+	elementSizing=0;
 	aboutMeCategories=["my introduction","recent work experience","basic and contact information"];
 	async aboutMeMovement(direction:string){
 		if(direction=="left" && this.scrollCount > 0){
-			this.scroller=this.scroller-document.getElementById("aboutMePanel")!.clientWidth;
+			this.scroller=(this.scrollCount - 1) * document.getElementById("aboutMePanel")!.clientWidth;
 			document.getElementById("aboutMePanel")!.scroll(this.scroller, 0);
 			this.scrollCount--;
+			console.log(document.getElementById("aboutMePanel")!.clientWidth);
 		}
 		else if(direction=="right" &&  this.scrollCount < 2){
-			this.scroller=this.scroller+document.getElementById("aboutMePanel")!.clientWidth;
+			this.scroller=(this.scrollCount + 1) * document.getElementById("aboutMePanel")!.clientWidth;
 			document.getElementById("aboutMePanel")!.scroll(this.scroller, 0);
 			this.scrollCount++;
+			console.log(document.getElementById("aboutMePanel")!.clientWidth);
 		}
 	}
 }
